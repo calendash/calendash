17f16c320d122b16b6c57f4fc9de7780be327bb0
rocmaz <ruben.omh@proton.me>
2025-06-20 15:58:06 -0600

    chore(core): initial repo setup
    Signed-off-by: rocmaz <ruben.omh@proton.me>

---
+++ packages/core/jest.config.mjs
@@ -0,0 +1,11 @@
+import { jestConfig } from "@calendash/shared";
+
+/** @type {import('jest').Config} */
+export default {
+  ...jestConfig,
+  preset: "ts-jest",
+  transform: {
+    "^.+\\.tsx?$": ["ts-jest", { tsconfig: "./tsconfig.spec.json" }],
+  },
+  testEnvironment: "node",
+};
