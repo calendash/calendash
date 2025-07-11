0f2725f329a2f76b5fab44e50b2ca50a23a57a92
rocmaz <ruben.omh@proton.me>
2025-07-10 22:09:35 -0600

    chore(config): add shared scripts
    Signed-off-by: rocmaz <ruben.omh@proton.me>

---
+++ config/scripts/build-api.mjs
@@ -0,0 +1,74 @@
+#!/usr/bin/env node
+'use strict';
+
+import yargs from 'yargs';
+import { hideBin } from 'yargs/helpers';
+import { $, chalk, echo, fs } from 'zx';
+import { glob } from 'glob';
+
+async function run({ tsConfig, aeConfig }) {
+	const tscPaths = await glob(tsConfig);
+	const aecPaths = await glob(aeConfig);
+
+	echo(
+		chalk.cyan(`→ Building TS file${tscPaths.length > 1 ? 's' : ''}:`) +
+			chalk.whiteBright(`\n\n\t${tscPaths.join(',\v\r\t  ')}\n`)
+	);
+
+	// Exec TypeScript build for each tsconfig file
+	await Promise.all(tscPaths.map((tscPath) => $`npx tsc -b ${tscPath}`)).catch((err) => {
+		echo(chalk.redBright(`✖ Failed to build TS`) + chalk.bgGray(`\n ${err} \n`));
+		process.exit(1);
+	});
+
+	echo(
+		chalk.cyan(`→ Running API Extractor${aecPaths.length > 1 ? 's' : ''}:`) +
+			chalk.whiteBright(`\n\n\t${aecPaths.join(',\v\r\t  ')}\n`)
+	);
+
+	// Exec API Extractor command for each config file
+	await Promise.all(
+		aecPaths.map(async (aecPath) => {
+			await $`npx api-extractor run --local --verbose -c ${aecPath}`;
+			const configFile = await fs.readJson(aecPath);
+			const filePath = configFile.dtsRollup?.untrimmedFilePath;
+			if (filePath) {
+				const path = filePath.replace('<projectFolder>', configFile.projectFolder ?? '.');
+				const dtsFile = await fs.readFile(path, 'utf-8');
+				if (dtsFile.includes('React_2')) {
+					echo(chalk.cyan(`→ Replacing "React_2" with "React" in `) + chalk.greenBright(path));
+					await fs.writeFile(path, dtsFile.replace(/React_2/g, 'React'));
+				}
+
+				const mdtsPath = path.replace(/\.d\.ts$/, '.d.mts');
+				echo(
+					chalk.cyan(
+						'→ Creating file ' + chalk.greenBright(mdtsPath) + ' out of ' + chalk.greenBright(path)
+					)
+				);
+				await fs.copyFile(path, mdtsPath);
+			}
+		})
+	).catch((err) => {
+		echo(chalk.redBright(`✖ Failed to run API extractor`) + chalk.bgGray(`\n ${err} \n`));
+		process.exit(1);
+	});
+
+	process.exit(0);
+}
+
+const { argv } = yargs(hideBin(process.argv))
+	.option('tsConfig', {
+		alias: 'tsc',
+		description: 'TypeScript build configuration file.',
+		type: 'string',
+		default: './tsconfig.json',
+	})
+	.option('aeConfig', {
+		alias: 'aec',
+		description: 'API Extractor configuration file.',
+		type: 'string',
+		default: './api-extractor.json',
+	});
+
+run(argv);
---
+++ config/scripts/fetch-exports.mjs
@@ -0,0 +1,81 @@
+#!/usr/bin/env node
+'use strict';
+
+import { fileURLToPath } from 'node:url';
+import { chalk, fs, path } from 'zx';
+
+async function validateExports(exports, rootPath) {
+	// Fetch misssing export files
+	const missingFiles = await Promise.all(
+		Object.values(exports).map((xport) => {
+			if (typeof xport === 'string') return [];
+			echo(
+				chalk.cyan(`→ Validating exports:`) + chalk.yellowBright(`\n\n${JSON.stringify(xport)}\n`)
+			);
+			return Promise.all([
+				fs.exists(xport.default),
+				fs.exists(xport.module),
+				fs.exists(xport.types),
+			]).then(([main, module, types]) =>
+				[
+					!main && path.relative(rootPath, xport.default),
+					!module && path.relative(rootPath, xport.module),
+					!types && path.relative(rootPath, xport.types),
+				].filter((value) => Boolean(value))
+			);
+		})
+	).then((files) => files.flat());
+
+	// Exit with error if missing files exist.
+	if (missingFiles.length > 0) {
+		echo(
+			chalk.redBright(`✖ Missing files:\n\n\t${missingFiles.join(',\v\r\t  ')}`) +
+				chalk.bgGray('\n Script failed. Did you forget to build? \n')
+		);
+		process.exit(1);
+	}
+
+	process.exit(0);
+}
+
+async function run() {
+	// Get project package "exports" section
+	const rootPath = path.resolve(path.dirname(fileURLToPath(import.meta.url)), '../..');
+	const { exports } = await fs.readJSON('./package.json');
+
+	echo(
+		chalk.cyan(`→ Fetching package.json exports from path:`) +
+			chalk.whiteBright(`\n\n\t${rootPath}\n`)
+	);
+
+	// Exit script with no error, if no exports
+	if (!exports) process.exit(0);
+
+	// Validate presence of export files
+	await validateExports(exports, rootPath);
+
+	// Copying files to specified path (except for root path)
+	for (const [key, xport] of Object.entries(exports)) {
+		if (path.extname(key) || typeof xport === 'string' || key === '.') continue;
+		const { default: main, module, types } = xport;
+		await fs.mkdirp(key);
+		await Promise.all([
+			fs.copyFile(main, `${key}/${path.basename(main)}`),
+			fs.copyFile(module, `${key}/${path.basename(module)}`),
+			fs.copyFile(types, `${key}/${path.basename(types)}`),
+			fs.writeJson(
+				`${key}/package.json`,
+				{
+					sideEffects: false,
+					main: path.basename(main),
+					module: path.basename(module),
+					types: path.basename(types),
+				},
+				{ spaces: 2 }
+			),
+		]);
+	}
+	process.exit(0);
+}
+
+run();
